1165e74505469bef7c599a2890f6f088a98cde34
Mariya Nikonova <mariia-nikonova@mail.ru>
2023-08-22 23:03:21 +0300

    task-41_commit

---
+++ task_41/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите числа через пробел: ");
+int[] number = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int count(int[] number)
+{
+    int n = number.Length;
+    int c = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (number[i] > 0) c = c + 1;
+    }
+    return c;
+}
+
+int k = count(number);
+Console.WriteLine($"Количество положительных элементов в массиве: {k}");
+
+
+
+
